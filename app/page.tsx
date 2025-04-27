c5972d6120dab0f6c2c4b1df75ba2c662429da1f
Ryan Burmeister-Morrison <rburmeistermorrison@gmail.com>
2025-04-26 23:05:40 -0400

    feat: use localStorage to remember the diagram type and code

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,62 @@ import {
 } from "@/components/ui/resizable";
 import DiagramTypeDropdown from "./_components/DiagramTypeDropdown";
 import { useState } from "react";
+import { Editor } from "@monaco-editor/react";
+import React from "react";
 
 export default function Home() {
 	const [diagramType, setDiagramType] = useState("mermaid");
+	const [diagramCode, setDiagramCode] = useState("flowchart TD\n    A --> B");
+
+	const onDiagramTypeChange = (value: string) => {
+		setDiagramType(value);
+		if (typeof window !== "undefined") {
+			localStorage.setItem("diagramType", value);
+		}
+	};
+
+	const onEditorChange = (value: string | undefined) => {
+		setDiagramCode(value || "");
+		if (typeof window !== "undefined") {
+			localStorage.setItem("diagramCode", value || "");
+		}
+	};
+
+	React.useEffect(() => {
+		if (typeof window !== "undefined") {
+			const storedType = localStorage.getItem("diagramType");
+			if (storedType) {
+				setDiagramType(storedType);
+			}
+
+			const storedCode = localStorage.getItem("diagramCode");
+			if (storedCode) {
+				setDiagramCode(storedCode);
+			}
+		}
+	}, []);
 
 	return (
 		<div className="w-screen h-screen">
 			<ResizablePanelGroup direction="horizontal">
 				<ResizablePanel minSize={20} defaultSize={35} maxSize={50}>
-					<div className="flex h-full p-4 flex-col">
+					<div className="flex gap-4 h-full p-4 flex-col bg-neutral-50">
 						<DiagramTypeDropdown
 							value={diagramType}
-							onValueChange={setDiagramType}
+							onValueChange={onDiagramTypeChange}
 						/>
+						<div className="rounded flex-1 border border-slate-200">
+							<Editor
+								value={diagramCode}
+								onChange={onEditorChange}
+								options={{ fontSize: 14 }}
+							/>
+						</div>
 					</div>
 				</ResizablePanel>
-				<ResizableHandle withHandle />
+				<ResizableHandle />
 				<ResizablePanel>
-					<div className="h-full flex justify-center items-center bg-orange-50">
-						Hello!
-					</div>
+					<div className="h-full flex justify-center items-center">Hello!</div>
 				</ResizablePanel>
 			</ResizablePanelGroup>
 		</div>
